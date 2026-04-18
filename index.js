bd0701edf2f91c48e4d008e070a75966368f4e83
Shashank01-hub <chauhan.shashank987@gmail.com>
2026-04-18 16:44:38 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get("/health", (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log("Server running on this port " + PORT);
+    console.log("Server running on port 3000");
 });
